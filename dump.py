93cf97f223beeb207ef1550b1c48a2e99b138839
Olly <78851163+sparkByProxy@users.noreply.github.com>
2025-09-09 21:58:28 +0200

    Format entry output to include title, username, and password

--- dump.py
+++ dump.py
@@ -2,4 +2,4 @@ from pykeepass import PyKeePass
 
 kp = PyKeePass('Database.kdbx', password='mercedes1')
 for entry in kp.entries:
-    print(entry)
+    print(f"{entry.title} = {entry.username}:{entry.password}")
